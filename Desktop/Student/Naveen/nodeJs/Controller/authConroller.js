9046e30193110090f57f44d42bbd8bc8fe7d9c9f
NAVEEN KUMAR <naveenkumardass011@gmail.com>
2025-06-19 22:35:54 +0530

    ADD:Forget-password route

--- Desktop/Student/Naveen/nodeJs/Controller/authConroller.js
+++ Desktop/Student/Naveen/nodeJs/Controller/authConroller.js
@@ -1,28 +1,40 @@
-const {UserModel } = require('../model/User');
-const ForgetPasswordList = require('../model/forgetPasswordList');
+const { UserModel } = require('../model/User');
+const { ForgetPasswordList } = require('../model/forgetPasswordList');
 
-const register=async(req,res)=>{
+const register = async (req, res) => {
     const data = req.body
     console.log(data);
     try {
         const newUser = await UserModel.create(data);
-        res.send({message:'User created successfully!'})
+        res.send({ message: 'User created successfully!' })
     } catch (error) {
-        res.status(500).send({message:"User created failed",success:true,error:error.message})
+        res.status(500).send({ message: "User created failed", success: false, error: error.message })
     }
 }
 
-const forgetPassword =async(req,res)=> {
+const forgetPassword = async (req, res) => {
     const data = req.body
     const { id } = req.params
-    const currentUser =await UserModel.findById(id)
-    console.log(currentUser);
-    const count = await ForgetPasswordList.find({ _id: id }).countDocuments()
-    console.log(count);
-    
-    
-    
-    res.send({message:'Password changed successfully!'})
+    if (!data.password) return res.send({ message: "Please provide password" });
+    if (!id) return res.status(400).send({ message: 'Please provite id', success: false });
+    try {
+        const currentUser = await UserModel.findOne({ _id: id })
+        if (!currentUser) {
+            return res.status(404).send({ message: 'User not found', success: true })
+        }
+        const count = await ForgetPasswordList.find({ userId: id }).countDocuments()
+        if (count < 5) {
+            await ForgetPasswordList.create({ changedpassword: data.password, changeCount: count + 1, userId: currentUser._id })
+            currentUser.password = data.password
+            await currentUser.save()
+            return res.send({ message: 'Password changed successfully!' })
+
+        }
+        res.send({ message: 'ForgetPassword limit exceed please try again later' })
+
+    } catch (error) {
+        res.status(500).send({ message: "Password change failed", success: false, error: error.toString() })
+    }
 }
 
-module.exports={register,forgetPassword}
+module.exports = { register, forgetPassword }
--- Desktop/Student/Naveen/nodeJs/model/forgetPasswordList.js
+++ Desktop/Student/Naveen/nodeJs/model/forgetPasswordList.js
@@ -1,25 +1,25 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
 
 const forgetPasswordSchema = new mongoose.Schema({
     userId: {
-        type:mongoose.Types.ObjectId,
-        required:true
+        type: mongoose.Types.ObjectId,
+        required: true
     },
     changedpassword: {
         type: String,
-        required:true
+        required: true
     },
-    changeCount:{
+    changeCount: {
         type: Number,
-        default:0
+        default: 0
     },
-    createdAt:{
+    createdAt: {
         type: Date,
-        default:Date.now()
+        default: Date.now()
     }
 })
 
 const ForgetPasswordList = mongoose.model('forgetPasswordList', forgetPasswordSchema)
 
-module.exports=ForgetPasswordList
+module.exports = { ForgetPasswordList }
